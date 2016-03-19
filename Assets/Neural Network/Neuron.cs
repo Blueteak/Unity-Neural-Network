c60b89130eaea2ef7e3d2b3e42c6f394a984c3d8
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2016-03-19 00:31:33 -0700

    Initial Commit

---
+++ Assets/Neural Network/Neuron.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace NeuralNetwork
+{
+
+	public class Neuron 
+	{
+		public List<Synapse> InputSynapses { get; set; }
+		public List<Synapse> OutputSynapses { get; set; }
+		public double Bias { get; set; }
+		public double BiasDelta { get; set; }
+		public double Gradient { get; set; }
+		public double Value { get; set; }
+
+		public Neuron()
+			{
+				InputSynapses = new List<Synapse>();
+				OutputSynapses = new List<Synapse>();
+				Bias = Network.GetRandom();
+			}
+
+			public Neuron(IEnumerable<Neuron> inputNeurons) : this()
+			{
+				foreach (var inputNeuron in inputNeurons)
+				{
+					var synapse = new Synapse(inputNeuron, this);
+					inputNeuron.OutputSynapses.Add(synapse);
+					InputSynapses.Add(synapse);
+				}
+			}
+
+			public virtual double CalculateValue()
+			{
+				return Value = Sigmoid.Output(InputSynapses.Sum(a => a.Weight * a.InputNeuron.Value) + Bias);
+			}
+
+			public double CalculateError(double target)
+			{
+				return target - Value;
+			}
+
+			public double CalculateGradient(double? target = null)
+			{
+				if(target == null)
+					return Gradient = OutputSynapses.Sum(a => a.OutputNeuron.Gradient * a.Weight) * Sigmoid.Derivative(Value);
+
+				return Gradient = CalculateError(target.Value) * Sigmoid.Derivative(Value);
+			}
+
+			public void UpdateWeights(double learnRate, double momentum)
+			{
+				var prevDelta = BiasDelta;
+				BiasDelta = learnRate * Gradient;
+				Bias += BiasDelta + momentum * prevDelta;
+
+				foreach (var synapse in InputSynapses)
+				{
+					prevDelta = synapse.WeightDelta;
+					synapse.WeightDelta = learnRate * Gradient * synapse.InputNeuron.Value;
+					synapse.Weight += synapse.WeightDelta + momentum * prevDelta;
+				}
+			}
+
+	}
+
+	public class Synapse
+	{
+		public Neuron InputNeuron { get; set; }
+		public Neuron OutputNeuron { get; set; }
+		public double Weight { get; set; }
+		public double WeightDelta { get; set; }
+
+		public Synapse(Neuron inputNeuron, Neuron outputNeuron)
+		{
+			InputNeuron = inputNeuron;
+			OutputNeuron = outputNeuron;
+			Weight = Network.GetRandom();
+		}
+	}
+
+	public static class Sigmoid
+	{
+		public static double Output(double x)
+		{
+			return x < -45.0 ? 0.0 : x > 45.0 ? 1.0 : 1.0 / (1.0 + Mathf.Exp((float)-x));
+		}
+
+		public static double Derivative(double x)
+		{
+			return x * (1 - x);
+		}
+	}
+
+	public class DataSet
+	{
+		public double[] Values { get; set; }
+		public double[] Targets { get; set; }
+
+		public DataSet(double[] values, double[] targets)
+		{
+			Values = values;
+			Targets = targets;
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+#Unity3D Neural Network
+This is a simple Backpropogation neural network.
+<br /><br />
+This project also has a demo scene showing the power of the network by using it to determine which color text is easist to read on randomly colored backgrounds.
+<br /><br />
+![Alt text](http://i.imgur.com/IBu2xU7.png)
+<br />
+
+Users can select the color easiest to read, and after a few trials (> 10) the Network will show its pick for 'easiest to read color' for the shown background.
+
