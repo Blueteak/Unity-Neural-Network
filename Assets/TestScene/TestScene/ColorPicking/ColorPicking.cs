6e9b5d734273a9b0263c732636c5713dae3dfad0
marvpaul <marvin.krueger@yahoo.de>
2017-09-12 17:31:29 +0200

    Added another test scene. Issue: Reloading feature does not reset the neural net

---
+++ Assets/TestScene/TestScene/ColorPicking/ColorPicking.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using NeuralNetwork;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ColorPicking : MonoBehaviour {
+
+	//Neural Network Variables
+	private const double MinimumError = 0.01;
+	private const TrainingType TrType = TrainingType.MinimumError;
+	private static NeuralNet net;
+	private static List<DataSet> dataSets; 
+
+	public Image I1;
+	public Image I2;
+
+	public GameObject pointer1;
+	public GameObject pointer2;
+
+	bool trained;
+
+	int i = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Input - 3 (r,g,b) -- Output - 1 (Black/White)
+		net = new NeuralNet(3, 4, 1);
+		dataSets = new List<DataSet>();
+		Next();
+	}
+
+	void Next()
+	{
+		Color c = new Color(Random.Range(0,1f), Random.Range(0,1f), Random.Range(0,1f));
+		I1.color = c;
+		I2.color = c;
+		double[] C = {(double)I1.color.r, (double)I1.color.g, (double)I1.color.b};
+		if(trained)
+		{
+			double d = tryValues(C);
+			if(d > 0.5)
+			{
+				pointer1.SetActive(false);
+				pointer2.SetActive(true);
+			}
+			else
+			{
+				pointer1.SetActive(true);
+				pointer2.SetActive(false);
+			}
+		}
+	}
+	
+	public void Train(float val)
+	{ 
+		double[] C = {(double)I1.color.r, (double)I1.color.g, (double)I1.color.b};
+		double[] v = {(double)val};
+		dataSets.Add(new DataSet(C, v));
+
+		i++;
+		if(!trained && i%10 == 9)
+			Train();
+
+		Next();
+
+	}
+
+	private void Train()
+	{
+		net.Train(dataSets, MinimumError);
+		trained = true;
+	}
+
+	double tryValues(double[] vals)
+	{
+	 	double[] result = net.Compute(vals);
+	 	return result[0];
+	}
+
+
+}
---
+++ Assets/TestScene/TestScene/Jump/SpawnNext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNext : MonoBehaviour {
+	bool wasSpawned = false;
+
+	public void OnTriggerEnter(Collider other){
+		if (!wasSpawned) {
+			GameObject newGround = Instantiate (Resources.Load ("Terrain") as GameObject);
+			newGround.transform.localScale = this.transform.localScale - Vector3.right * this.transform.localScale.x + Random.Range (8, 12F) * Vector3.right; 
+			newGround.transform.position = this.transform.position + (Vector3.right * this.transform.localScale.x) + Random.Range (1, 10) / 5 * Vector3.right + newGround.transform.localScale.x / 4 * Vector3.right; 
+			wasSpawned = true; 
+		} 
+	}
+}
